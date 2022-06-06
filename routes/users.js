e7522f56c782f2db48d9abd66cd82404e74dded1
‘ureshinoSayumi’ <l407v09@gmail.com>
2022-06-06 14:40:34 +0800

    去除多餘註解

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,19 @@ const userController = require('../controllers/user')
 const { isAuth,generateSendJWT } = require('../service/auth');
 
 /* GET users listing. */
-router.get('/users', isAuth, userController.getAllUser); // 取得所有會員
-router.post('/user/sign_up', userController.sign_up); // 註冊會員
-router.post('/users/sign_in', userController.sign_in); // 登入會員
-router.get('/users/profile', isAuth, userController.profile) // 取得個人資料
-router.post('/users/updatePassword', isAuth, userController.updatePassword) // 重設密碼
-router.patch('/users/updateProfile', isAuth, userController.updateProfile) // 更新個人資料
-router.get('/users/getLikeList', isAuth, userController.getLikeList) // 取得個人按讚列表
-router.get('/users/following', isAuth, userController.following) // 取得個人追蹤名單
+router.get('/users', isAuth, userController.getAllUser);
+router.post('/user/sign_up', userController.sign_up);
+router.post('/users/sign_in', userController.sign_in);
+router.get('/users/profile', isAuth, userController.profile)
+router.post('/users/updatePassword', isAuth, userController.updatePassword)
+router.patch('/users/updateProfile', isAuth, userController.updateProfile)
+router.get('/users/getLikeList', isAuth, userController.getLikeList)
+router.get('/users/following', isAuth, userController.following)
 
-router.get('/users/:id', isAuth, userController.getUser); // 取得單一會員
+router.get('/users/:id', isAuth, userController.getUser);
 
-router.post('/users/:id/follow', isAuth, userController.follow) // 追蹤朋友
-router.delete('/users/:id/unfollow', isAuth, userController.unfollow) // 取消追蹤朋友
-// following
+router.post('/users/:id/follow', isAuth, userController.follow)
+router.delete('/users/:id/unfollow', isAuth, userController.unfollow)
 
 
 module.exports = router;
